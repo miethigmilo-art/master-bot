7f67e75486622463f40682b5eed3c979f1885988
miethigmilo-art <miethigmilo@gmail.com>
2026-05-15 11:51:47 +0200

    chore: update master-bot

--- git_push.bat
+++ git_push.bat
@@ -5,7 +5,7 @@ if exist .git\index.lock del /f .git\index.lock
 git config user.email "miethigmilo@gmail.com"
 git config user.name "miethigmilo-art"
 git add -A
-git commit -m "feat: Dynamic Confidence Threshold per Market Mode (V2)"
+git commit -m "chore: update master-bot"
 git push https://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@github.com/miethigmilo-art/master-bot.git main
 
 echo.
@@ -16,8 +16,9 @@ if exist .git\HEAD.lock del /f .git\HEAD.lock
 if exist .git\index.lock del /f .git\index.lock
 git config user.email "miethigmilo@gmail.com"
 git config user.name "miethigmilo-art"
+git pull --rebase https://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@github.com/miethigmilo-art/ml-service.git main
 git add -A
-git commit -m "feat: Dynamic threshold from master-bot (Market Mode aware)"
+git commit -m "chore: update ml-service"
 git push https://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@github.com/miethigmilo-art/ml-service.git main
 
 echo.
